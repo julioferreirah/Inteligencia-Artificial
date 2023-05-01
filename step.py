22fc7a572eea8e95475445497bc2cb32593d7833
Júlio Ferreira <86128073+julioferreirah@users.noreply.github.com>
2023-04-30 21:59:01 -0300

    Desenvolvimento Perceptron Simples (30/4)

---
+++ step.py
@@ -0,0 +1,5 @@
+def step(x):
+    if x > 0:
+        return 1
+    else:
+        return 0
---
+++ teste.py
@@ -0,0 +1,4 @@
+import numpy as np
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+print(X.shape[0])
+print(X[2][1])
